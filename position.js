fa903abf763512ba926e3078625ec872c9390453
mike31s <michael88spencer@gmail.com>
2015-09-23 20:40:20 -1000

    finished in 12 min

--- position.js
+++ position.js
@@ -1,4 +1,18 @@
 /**
  * Created by mike on 9/23/2015.
  */
-console.log("hello");
+var nums = _.sortBy([2,6,4,3,7,9]);
+
+console.log(nums);
+
+function position(array, index)
+{
+    if( _.indexOf(array, index) === -1)
+        return("number not found");
+    else
+        return _.indexOf(array, index);
+}
+
+console.log("six: " + position(nums, 6));
+console.log("2: " + position(nums, 2));
+console.log("10: " + position(nums, 10));
