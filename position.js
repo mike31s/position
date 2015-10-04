79b81bf8400f8839321f08d8ff6ccf4a983e669e
mike31s <michael88spencer@gmail.com>
2015-10-03 19:13:24 -1000

    updated to match example

--- position.js
+++ position.js
@@ -1,18 +1,22 @@
 /**
  * Created by mike on 9/23/2015.
  */
-var nums = _.sortBy([2,6,4,3,7,9]);
+var nums = _.sortBy([2, 6, 4, 3, 7, 9]);
 
 console.log(nums);
 
-function position(array, index)
-{
-    if( _.indexOf(array, index) === -1)
-        return("number not found");
-    else
-        return _.indexOf(array, index);
+function position(nums, num) {
+  var cursor = 0;
+  while ((nums[cursor] <= num) && (cursor < nums.length)) {
+    if (nums[cursor] === num) {
+      return cursor;
+    }
+    cursor++;
+  }
+  return cursor;
 }
 
+var testnums[1,3,5,6];
 console.log("six: " + position(nums, 6));
 console.log("2: " + position(nums, 2));
 console.log("10: " + position(nums, 10));
